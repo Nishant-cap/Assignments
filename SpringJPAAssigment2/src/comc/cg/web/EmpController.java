c98118d7bcffc7a52ec51cdfd726efd5c6bb08ce
Nishant-cap <36667413+Nishant-cap@users.noreply.github.com>
2020-04-15 13:48:00 +0530

    Assignment2

---
+++ SpringJPAAssigment2/src/comc/cg/web/EmpController.java
@@ -0,0 +1,46 @@
+package comc.cg.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.cg.dao.IEmpDao;
+
+@Controller
+public class EmpController {
+
+	@Autowired
+	private IEmpDao dao;
+
+	@GetMapping("viewall")
+	public String viewall(Model model) {
+		model.addAttribute("elist", dao.viewEmp());
+		return "EmpviewPage";
+	}
+
+	@GetMapping("/viewbyid")
+	public String viewByID() {
+		return "ViewIdPage";
+	}
+
+	@GetMapping("/searchbyid")
+	public String searchByID(@RequestParam("texteid") int eid, Model model) {
+		model.addAttribute("emodel", dao.viewEmp(eid));
+		return "ViewIdPage";
+	}
+
+	@GetMapping("/viewbydept")
+	public String viewByDept(Model model) {
+		model.addAttribute("dlist", dao.viewDepts());
+		return "ViewDeptPage";
+	}
+
+	@GetMapping(value = "/searchbydept")
+	public String searchByDept(@RequestParam("txtdept") String dept, Model model) {
+		model.addAttribute("elist", dao.viewEmpByDept(dept));
+		return "EmpviewPage";
+	}
+
+}
