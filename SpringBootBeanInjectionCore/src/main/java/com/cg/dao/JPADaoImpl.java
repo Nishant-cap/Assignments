877bb8df6ed5686ca6c073d72c7b3f75ee78bd95
Nishant-cap <36667413+Nishant-cap@users.noreply.github.com>
2020-04-15 14:24:29 +0530

    Assignment 5-1

---
+++ SpringBootBeanInjectionCore/src/main/java/com/cg/dao/JPADaoImpl.java
@@ -0,0 +1,20 @@
+package com.cg.dao;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component("jpadao")
+@Lazy(true)
+public class JPADaoImpl implements IDao{
+	
+	public JPADaoImpl() {
+		System.out.println("jpa dao constructor");
+	}
+
+	@Override
+	public String getName() {
+		
+		return "jpa name";
+	}
+
+}
